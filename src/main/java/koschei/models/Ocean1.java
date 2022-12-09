5de9273b61a1dfa36dd03f321aae4bd5a48b3e8c
Alexandr <sasharudchenko@yandex.ru>
2022-12-09 17:49:53 +0300

    2.1.5

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -1,13 +1,18 @@
 package koschei.models;
 
+import koschei.KoscheiTheDeathless;
+import koschei.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Ocean1 {
-
+public class Ocean1  {
+private AppConfig appConfig;
     @Autowired
     private Island2 island;
+//    public void setIsland (Island2 island) {
+//        this.island = island;
+//    }
 
     @Override
     public String toString() {
